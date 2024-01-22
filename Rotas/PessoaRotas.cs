c3f949ecdcee08424a1e4eca4e2eaadd9f0d2d3b
daniel.paula <101979436+danieldpa@users.noreply.github.com>
2024-01-22 12:34:48 -0300

    Feito método PUT

--- Rotas/PessoaRotas.cs
+++ Rotas/PessoaRotas.cs
@@ -15,14 +15,26 @@ public static class PessoaRotas
 
         app.MapGet("/pessoas/{nome}", (string nome) => Pessoas.Find(x => x.Nome == nome));
 
-        app.MapPost("pessoas", (Pessoa pessoa) =>
-        {   
+        app.MapPost("/pessoas", (Pessoa pessoa) =>
+        {
             if (pessoa.Nome == "Daniel")
             {
-                return Results.BadRequest( new {message = "Não aceitamos nome Daniel"}); //Como se fosse o try/cath
+                return Results.BadRequest(new { message = "Não aceitamos nome Daniel" }); //Como se fosse o try/cath
             }
             Pessoas.Add(pessoa);
             return Results.Ok(pessoa);
+        });
+
+        app.MapPut("/pessoas/{id}", (Guid id, Pessoa pessoa) =>
+        {
+            var encontrado = Pessoas.Find(x => x.Id == id);
+
+            if(encontrado == null)
+                return Results.NotFound();
+            
+            encontrado.Nome = pessoa.Nome;
+
+            return Results.Ok(encontrado);
         });
     }
 }
--- obj/Debug/net8.0/PrimeiraApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimeiraApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiraApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7310dcc801a1ec279565d01ab0bbc0ec1abfeba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiraApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiraApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
